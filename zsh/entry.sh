0a6e4c1e70bbc44da3bd846016d0b436a1a923e4
Cloud Le <lehaoson@live.com>
2021-10-18 10:53:03 +0700

    oh-my-zsh, common zsh configs in dotfiles

--- zsh/entry.sh
+++ zsh/entry.sh
@@ -1,3 +1,23 @@
-chpwd() ls
+export LANG=en_US.UTF-8
+export ZSH="/Users/le/.oh-my-zsh"
+# export NVM_LAZY_LOAD=true
+export NVM_COMPLETION=true
 
+plugins=(
+	git
+	zsh-nvm
+	zsh-autosuggestions
+	zsh-syntax-highlighting
+)
+
+source $ZSH/oh-my-zsh.sh
+source ~/dotfiles/zsh/nvmrc.sh
 source ~/dotfiles/zsh/keybindings.sh
+
+bindkey -v
+alias vim="nvim"
+# chpwd() ls
+
+export STARSHIP_CONFIG=~/dotfiles/conf/starship.toml
+eval "$(starship init zsh)" # load starship theme
+
